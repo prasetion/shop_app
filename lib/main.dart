5f68b349a99ac1d58a75f91d83463118955ae784
prasetion <prasetion@gmail.com>
2022-05-30 17:01:19 +0700

    adding form, user product screen, edit product screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
+import 'package:shop_app/screens/user_products_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import 'package:provider/provider.dart';
@@ -32,6 +34,8 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
+import 'package:shop_app/widgets/user_product_item.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = "/user-products";
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Products"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+          itemCount: productsData.items.length,
+        ),
+      ),
+      drawer: AppDrawer(),
+    );
+  }
+}
