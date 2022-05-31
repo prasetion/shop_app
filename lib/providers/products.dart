5d0e4dfae09ceaa46977ed4c8f2197f0468d8882
prasetion <prasetion@gmail.com>
2022-05-31 21:15:56 +0700

    delete product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -82,4 +82,9 @@ class Products with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((prod) => prod.id == id);
+    notifyListeners();
+  }
 }
